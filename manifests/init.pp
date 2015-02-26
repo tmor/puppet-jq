4407492bdd705baef904761148c957572d015c12
Rick Fletcher <fletch@pobox.com>
2015-02-26 12:26:17 -0500

    Make it possible to upgrade

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,9 @@
 # [*ensure*]
 #   present, absent
 #
+# [*latest*]
+#   The latest version of jq available at $source.
+#
 # [*source*]
 #   The URL of the jq binary
 #
@@ -20,23 +23,47 @@
 #
 # === Copyright
 #
-# Copyright 2014 Rick Fletcher
+# Copyright 2015 Rick Fletcher
 #
 class jq (
   $ensure = present,
-  $source = 'http://stedolan.github.io/jq/download/linux64/jq',
+  $latest = '1.4',
+  $source = undef,
 ) {
-  if $ensure == 'present' {
-    wget::fetch { $source:
-      source      => $source,
-      destination => '/usr/local/bin/jq',
+  $version = $ensure ? {
+    present => $latest,
+    absent  => $latest,
+    latest  => $latest,
+    default => $ensure,
+  }
+
+  if $ensure == present or $ensure == $latest {
+    $url = 'http://stedolan.github.io/jq/download/linux64/jq'
+  } else {
+    $url = "http://stedolan.github.io/jq/download/linux64/jq-${version}/jq"
+  }
+
+  $binary_path = "/usr/local/bin/jq-${version}"
+
+  if $ensure != 'absent' {
+    $real_source = $source ? { undef => $url, default => $source }
+
+    wget::fetch { $real_source:
+      source      => $real_source,
+      destination => $binary_path,
       timeout     => 0,
       verbose     => false,
+      before      => File[$binary_path],
     }
   }
 
-  file { '/usr/local/bin/jq':
-    ensure => $ensure,
+  file { $binary_path:
+    ensure => $ensure ? { absent => $ensure, default => present },
     mode   => '0755',
+  } ->
+
+  file { '/usr/local/bin/jq':
+    ensure => $ensure ? { absent => $ensure, default => link },
+    target => $binary_path,
   }
 }
